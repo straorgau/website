e9c912fd941255d0d37955b49624ca5925e1ff7a
Duale Siad <me@wale.id.au>
2025-03-31 11:34:23 +1100

    refactor(nav+hero): remove daisyui elements, add `aria-label` to icons

--- components/nav/Dropdown.vue
+++ components/nav/Dropdown.vue
@@ -2,8 +2,8 @@
   <div>
     <button class="flex flex-row gap-2 items-center" @click="show = !show">
       <span class="text-xl font-semibold">{{title}}</span>
-      <Icon v-if="!show" name="gg:chevron-down" size="20" />
-      <Icon v-else name="gg:chevron-up" size="20" />
+      <Icon v-if="!show" aria-label="Open" name="gg:chevron-down" size="20" />
+      <Icon v-else aria-label="Close" name="gg:chevron-up" size="20" />
     </button>
     <div v-show="show" class="fixed py-4 mt-2 mr-16 rounded-md shadow-xl w-fit border-2 bg-bg-dark border-fg-dark">
       <div class="flex flex-col gap-4 px-4">
--- components/nav/MobileDropdown.vue
+++ components/nav/MobileDropdown.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <button class="right-0" @click="show = !show">
-      <Icon v-if="!show" name="gg:menu" size="32" />
-      <Icon v-else name="gg:close" size="32" />
+      <Icon v-if="!show" aria-label="Open" name="gg:menu" size="32" />
+      <Icon v-else aria-label="Close" name="gg:close" size="32" />
     </button>
     <div v-show="show" class="z-4 absolute top-32 left-0 w-full p-4 flex flex-col bg-bg-dark opacity-100 items-center rounded-b-sm border-b-fg-dark border-b-4 gap-8">
       <slot />
