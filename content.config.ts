4d588400809d42f440f0b7099cec084cf1f394c6
Duale Siad <me@wale.id.au>
2025-05-12 14:43:17 +1000

    fix: remove codeberg nuxt-content driver

--- content.config.ts
+++ content.config.ts
@@ -1,17 +1,11 @@
 import { defineCollection, defineContentConfig } from "@nuxt/content";
-import { forgejoContentDriver } from "./lib/unstorage/forgejoDriver";
 
 
 export default defineContentConfig({
   collections: {
     policies: defineCollection({
       type: "page",
-      source: forgejoContentDriver({
-        host: "https://codeberg.org",
-        basePath: "/api/v1",
-        repo: "stra/policies",
-        exclude: ["README.md"]
-      }),
+      source: "policies/*.md",
     })
   }
 })
