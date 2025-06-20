7216507deff0720036f7c3795b72cf403eb31562
Duale Siad <me@wale.id.au>
2025-06-21 00:37:38 +1000

    fix(devenv): add packages to Nix flake

--- flake.nix
+++ flake.nix
@@ -1,4 +1,26 @@
 {
-  inputs.nix-nvm.url = "github:jim3692/nix-nvm";
-  outputs = { nix-nvm, ... }: nix-nvm;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-nvm.url = "github:jim3692/nix-nvm";
+  };
+
+  outputs = {nixpkgs, ...} @ inputs: let
+    forSystems = fn:
+      nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ] (system: fn nixpkgs.legacyPackages.${system});
+    defaultForSystems = fn: forSystems (pkgs: {default = fn pkgs;});
+  in {
+    devShells = defaultForSystems (
+      pkgs:
+        with pkgs;
+          mkShell {
+            nativeBuildInputs = [flyctl];
+            inputsFrom = [inputs.nix-nvm.devShells.${system}.default];
+          }
+    );
+  };
 }
