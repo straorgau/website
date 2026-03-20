4a0b0105fba509fb63a9e09f044a33eed16e2ffc
Duale Siad <me@wale.id.au>
2026-03-20 15:22:27 +1100

    chore: remove temporary link to events page

--- server/api/events.ts
+++ server/api/events.ts
@@ -3,7 +3,7 @@ import { parseAtomFeed } from "feedsmith";
 // This simple parser is required to be done server-side, as TidyHQ has CORS enabled,
 // effectively rendering client-side Atom parsing impossible.
 export default defineEventHandler(async (event) => {
-  const res = await fetch("https://auscycling.tidyhq.com/public/schedule/events.atom");
+  const res = await fetch("https://stra.tidyhq.com/public/schedule/events.atom");
   const feedText = await res.text();
 
   const feed = parseAtomFeed(feedText);
