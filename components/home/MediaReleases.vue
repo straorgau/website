aa68bb4d21f631a127dc31b1ba2bfe3c5fb757ad
Duale Siad <me@wale.id.au>
2026-03-31 10:40:19 +1100

    fix: use `flex-col` for media releases by default

--- components/home/MediaReleases.vue
+++ components/home/MediaReleases.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="bg-bg-dark text-fg-dark justify-center w-full p-8 flex flex-col gap-4">
     <h1 class="text-3xl font-bold">Media Releases</h1>
-    <div class="flex flex-row gap-4 justify-center self-center">
+    <div class="flex flex-col lg:flex-row gap-4 justify-center self-center">
       <div v-for="release in releases" :key="release.id" class="flex flex-col p-6 bg-bg text-bg-dark rounded-sm gap-2 max-w-lg">
           <a :href="release.path" class="text-xl font-bold">{{ release.title }}</a>
           <span class="text-lg font-light">Last updated {{ formatDate(release.lastUpdated) }}.</span>
