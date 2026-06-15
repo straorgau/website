f47944e06ec20606eed0c9eb77dc0754eb7be38d
Duale Siad <me@wale.id.au>
2026-06-16 00:24:11 +1000

    fix: set navbar icons to `v-show`

--- components/nav/Dropdown.vue
+++ components/nav/Dropdown.vue
@@ -2,8 +2,8 @@
   <div>
     <button @click.prevent="show = !show" class="flex flex-row gap-2 items-center">
       <span class="text-xl font-semibold">{{title}}</span>
-      <Icon v-if="!show" aria-label="Open" name="gg:chevron-down" size="20" />
-      <Icon v-else aria-label="Close" name="gg:chevron-up" size="20" />
+      <Icon v-show="!show" aria-label="Open" name="gg:chevron-down" size="20" />
+      <Icon v-show="show" aria-label="Close" name="gg:chevron-up" size="20" />
     </button>
     <div v-show="show" ref="target" class="absolute py-4 mt-2 rounded-md shadow-xl self-auto border-2 bg-bg-dark border-fg-dark">
       <div class="flex flex-col gap-4 px-6">
